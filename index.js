5a4dbe40c8f2a014067be7132dc3c6ff952d8a1c
Astrit Spancs <astritspancs@Astrits-MacBook-Pro.local>
2022-10-08 03:25:05 +0200

    added web worker

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('App Started')
+if (window.Worker) {
+  const myWorker = new Worker("./worker.js");
+
+  document.getElementById("button").addEventListener("click", () => {
+    myWorker.postMessage("Message 1");
+  });
+
+  myWorker.onmessage = (e) => {
+    console.log(e)
+  }
+}
---
+++ worker.js
@@ -0,0 +1,4 @@
+onmessage = (e) => {
+  console.log(e);
+  postMessage("Thanks for the message");
+};
